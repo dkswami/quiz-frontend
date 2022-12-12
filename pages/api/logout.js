8243f7b55bd5e3cb62ad6a564f9ec8aa32d72d2e
dkswami <dkswami1820@gmail.com>
2022-12-13 01:11:52 +0530

    cookie env added

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,7 +3,7 @@ import cookie from 'cookie';
 export default function logoutHandler(req, res) {
 	res.setHeader('Set-Cookie', cookie.serialize('token', '', {
 		httpOnly: true,
-		secure: process.env.NODE_ENV !== 'development',
+		secure: process.env.NODE_ENV,
 		expires: new Date(0),
 		sameSite: 'strict',
 		path: '/',
